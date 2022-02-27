d0d24b84e80c8af5ac29b59428fe5f42e32d4702
manal <manalaboelez550@gmail.com>
2022-02-27 17:51:03 +0200

    autism

---
+++ database/migrations/2022_02_27_152713_create_parents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('userName');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->string('child_name');
+            $table->integer('child_age');
+            $table->binary('child_image');
+            $table->boolean('child_gender');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parents');
+    }
+}
---
+++ database/migrations/2022_02_27_153148_create_centers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCentersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('centerName');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->integer('photo_id')->unsigned();
+            $table->foreign('photo_id')->references('id')->on('center_photos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centers');
+    }
+}
